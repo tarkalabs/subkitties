e0597dde43cc17fc9404f5e54210cc86686a5d42
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2021-03-01 13:45:53 -0400

    added a module and verified that it worked.
    https://www.shawntabrizi.com/substrate-collectables-workshop/#/1/creating-a-module

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -41,6 +41,7 @@ use pallet_transaction_payment::CurrencyAdapter;
 
 /// Import the template pallet.
 pub use pallet_template;
+pub use pallet_subkitties;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -264,6 +265,10 @@ impl pallet_template::Config for Runtime {
 	type Event = Event;
 }
 
+impl pallet_subkitties::Config for Runtime {
+	type Event = Event;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -281,6 +286,7 @@ construct_runtime!(
 		Sudo: pallet_sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the template pallet in the runtime.
 		TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+		SubKitties: pallet_subkitties::{Module, Call, Storage, Event<T>}
 	}
 );
 
